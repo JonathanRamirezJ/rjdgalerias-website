9b79a53f8d412966a16e043b9d7f8c873f506c14
JonathanRamirez <jonajramirez@gmail.com>
2018-06-19 11:56:36 -0500

    assets: Add images in repo

--- src/components/card/CardsComp.js
+++ src/components/card/CardsComp.js
@@ -5,7 +5,7 @@ class CardsComp extends Component {
   render() {
     return (
       <Card
-        header={<CardTitle reveal image={"https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/"} waves='light'/>}
+        header={<CardTitle reveal image={"http://placehold.it/800x400/"} waves='light'/>}
         title="Card Title"
         reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
         <p><a href="#link">This is a link</a></p>
--- src/components/carousel/CarouselSlide.js
+++ src/components/carousel/CarouselSlide.js
@@ -8,11 +8,11 @@ class CarouselSlide extends Component {
         fixedItem={<button className='btn'>MORE</button>}
         options={{ fullWidth: true, duration: 200, indicators: true }}
         images={[
-          'https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/',
-          'https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/',
-          'https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/',
-          'https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/',
-          'https://www.hd-wallpapersdownload.com/download/720-hd-tv-hd-wallpaper-800x400/'
+          'http://placehold.it/800x400/',
+          'http://placehold.it/800x400/',
+          'http://placehold.it/800x400/',
+          'http://placehold.it/800x400/',
+          'http://placehold.it/800x400/'
         ]}
         />
     );
