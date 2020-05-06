5a13e7f786ecc2b77d81db010351b6f42c472aa2
Jonathan Ramirez <jonajramirez@gmail.com>
2020-05-06 15:19:43 -0500

    fix: Changes function component to class component

--- src/app/components/CarosuelImage.js
+++ src/app/components/CarosuelImage.js
@@ -1,47 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Carousel } from "react-bootstrap";
 
-const CarosuelImage = () => (
-  <Carousel controls={false}>
-    <Carousel.Item>
-      <img
-        width={1200}
-        height={500}
-        alt="1200x500"
-        src="./app/assets/wallpaper/fondo05.jpg"
-      />
-      <Carousel.Caption>
-        <h3>RJD Galerias</h3>
-        <p>Espacio musical.</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        width={1200}
-        height={500}
-        alt="1200x500"
-        src="./app/assets/wallpaper/fondo02.jpg"
-      />
-      <Carousel.Caption>
-        <h3>Cerca de ti</h3>
-        <p>
-          Ven y conoce nuestra gran variedad de productos que tenemos para ti.
-        </p>
-      </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        width={1200}
-        height={500}
-        alt="1200x500"
-        src="./app/assets/wallpaper/fondo03.jpg"
-      />
-      <Carousel.Caption>
-        <h3>¿Necesitas algo?</h3>
-        <p>Lo que buscas lo encuentras aquí</p>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
-);
+class CarosuelImage extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      document.getElementById("videoID").play();
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <Carousel controls={false} indicators={false}>
+        <Carousel.Item className="text-center">
+          <video
+            id="videoID"
+            preload="auto"
+            loop="loop"
+            muted="muted"
+            width={912}
+            poster="./app/assets/wallpaper/fondo05.jpg"
+          >
+            <source src="./app/assets/video/rjdVideo.mp4" type="video/mp4" />
+            Video not Support in your browser.
+            <track />
+          </video>
+          <Carousel.Caption>
+            <h3>RJD Galerias</h3>
+            <p>Tu espacio musical.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+}
 
 export default CarosuelImage;
